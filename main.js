9da56fc03409bc1a67102a00abc59b727bc9b383
nazaryan0909 <alenn0717@gmail.com>
2024-05-28 23:51:13 -0700

    avelacrel em mi functia vory array a gumarum

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ return a + b
 }
 let x = sum(5,6)
 console.log(x)
+
+function r(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+      sum = sum + arr[i]
+    }
+    return sum
+}
+console.log(r([2, 1, 3, 4]))
